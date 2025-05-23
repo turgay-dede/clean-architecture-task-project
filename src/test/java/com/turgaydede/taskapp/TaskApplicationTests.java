01eb3c7bccf680fb80126e09a7aa880fa29ea4fa
Turgay Dede <dev.turgaydede@gmail.com>
2025-05-23 17:07:48 +0300

    initial commit

---
+++ src/test/java/com/turgaydede/taskapp/TaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.turgaydede.taskapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
