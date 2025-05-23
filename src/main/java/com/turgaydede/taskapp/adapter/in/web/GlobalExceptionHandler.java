02989bc0b03ef72b3f0257b68de2bde6adb920e9
Turgay Dede <dev.turgaydede@gmail.com>
2025-05-23 22:23:43 +0300

    refactored

--- src/main/java/com/turgaydede/taskapp/adapter/in/web/GlobalExceptionHandler.java
+++ src/main/java/com/turgaydede/taskapp/adapter/in/web/GlobalExceptionHandler.java
@@ -6,11 +6,24 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.time.LocalDateTime;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
+    private ResponseEntity<Object> buildErrorResponse(String message, HttpStatus status) {
+        Map<String, Object> errorBody = new LinkedHashMap<>();
+        errorBody.put("timestamp", LocalDateTime.now());
+        errorBody.put("status", status.value());
+        errorBody.put("error", status.getReasonPhrase());
+        errorBody.put("message", message);
+
+        return ResponseEntity.status(status).body(errorBody);
+    }
     @ExceptionHandler(TaskNotFoundException.class)
     public ResponseEntity<?> handleNotFound(TaskNotFoundException ex) {
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+        return buildErrorResponse(ex.getMessage(), HttpStatus.NOT_FOUND);
     }
 }
